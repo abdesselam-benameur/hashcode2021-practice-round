a32b79de3556ea8eb399ee7a3710748d0967fda2
Abdesselam Benameur <ia_benameur@esi.dz>
2021-02-20 22:05:01 +0100

    solution based on finding pseudo best match

--- script.py
+++ script.py
@@ -1,52 +1,59 @@
-import datetime 
-alphabets = [str(x)for x in range(10000000)] 
-a = datetime.datetime.now() # store initial time 
-for item in alphabets: 
-    len(item) 
-b = datetime.datetime.now() # store final time 
-print((b-a).total_seconds()) # results 
-a = datetime.datetime.now() 
-fn = len                    # function stored locally 
-for item in alphabets: 
-    fn(item) 
-b = datetime.datetime.now() 
-print((b-a).total_seconds())
+line1 = input().split(' ')
+#M, T2, T3, T4, = *line1
 
 
-# import datetime
 
-# alphabets = [str(x) for x in range(10000000)]
-# a = datetime.datetime.now()  # store initial time
-# for item in alphabets:
-#     len(item)
-# b = datetime.datetime.now()  # store final time
-# print((b - a).total_seconds())  # results
-# a = datetime.datetime.now()
-# fn = len  # function stored locally
-# for item in alphabets:
-#     fn(item)
-# b = datetime.datetime.now()
-# print((b - a).total_seconds())
 
-# import datetime
-# import itertools
 
-# a = datetime.datetime.now()  # store initial time
-# combinations1 = [(x,y) for x in range(100000) for y in range(100000)]
-# b = datetime.datetime.now()  # store final time
-# print((b - a).total_seconds())  # results
+# import datetime 
+# alphabets = [str(x)for x in range(10000000)] 
+# a = datetime.datetime.now() # store initial time 
+# for item in alphabets: 
+#     len(item) 
+# b = datetime.datetime.now() # store final time 
+# print((b-a).total_seconds()) # results 
+# a = datetime.datetime.now() 
+# fn = len                    # function stored locally 
+# for item in alphabets: 
+#     fn(item) 
+# b = datetime.datetime.now() 
+# print((b-a).total_seconds())
 
-# combinations1  = []
-# a = datetime.datetime.now()
-# combinations1  = []
-# for x in range(100000):
-#     for y in range(100000):
-#         combinations1.append((x,y))
-# b = datetime.datetime.now()
-# print((b - a).total_seconds())
 
-# combinations1  = []
-# a = datetime.datetime.now()
-# combinations1 = itertools.combinations(range(100000), 2)
-# b = datetime.datetime.now()
-# print((b - a).total_seconds())
+# # import datetime
+
+# # alphabets = [str(x) for x in range(10000000)]
+# # a = datetime.datetime.now()  # store initial time
+# # for item in alphabets:
+# #     len(item)
+# # b = datetime.datetime.now()  # store final time
+# # print((b - a).total_seconds())  # results
+# # a = datetime.datetime.now()
+# # fn = len  # function stored locally
+# # for item in alphabets:
+# #     fn(item)
+# # b = datetime.datetime.now()
+# # print((b - a).total_seconds())
+
+# # import datetime
+# # import itertools
+
+# # a = datetime.datetime.now()  # store initial time
+# # combinations1 = [(x,y) for x in range(100000) for y in range(100000)]
+# # b = datetime.datetime.now()  # store final time
+# # print((b - a).total_seconds())  # results
+
+# # combinations1  = []
+# # a = datetime.datetime.now()
+# # combinations1  = []
+# # for x in range(100000):
+# #     for y in range(100000):
+# #         combinations1.append((x,y))
+# # b = datetime.datetime.now()
+# # print((b - a).total_seconds())
+
+# # combinations1  = []
+# # a = datetime.datetime.now()
+# # combinations1 = itertools.combinations(range(100000), 2)
+# # b = datetime.datetime.now()
+# # print((b - a).total_seconds())
