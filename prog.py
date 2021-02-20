dae3a980a35780a6b2151af679a28291fdc14c93
Abdesselam Benameur <ia_benameur@esi.dz>
2021-02-20 22:05:01 +0100

    solution based on finding pseudo best match

--- prog.py
+++ prog.py
@@ -35,7 +35,7 @@ for nameOfFile in names:
             ingredients = line[1:]
             pizzas.append(ingredients)
         
-        pizzasSorted = sorted(enumerate(pizzas), key=lambda e: len(e[1]))
+        pizzasSorted = sorted(enumerate(pizzas), key=lambda e: len(e[1]), reverse=True)
  
     try:
         bestScore = int(
@@ -43,73 +43,98 @@ for nameOfFile in names:
     except IndexError:
         bestScore = 0
 
-    values = list(range(0, numberOfIterations, step))
-    values.append(numberOfIterations)
+    # values = list(range(0, numberOfIterations, step))
+    # values.append(numberOfIterations)
 
-    for i in range(numberOfIterations):
-        if i == values[0]:
-            values.pop(0)
-            print("iteration {}".format(i))
-        b = 0
-        teamsCopy = teams.copy()
-        N = M
-        # pizzasIndices = list(range(N))
-        score = 0
-        deliveries = []
-        choices = [2, 3, 4]
-        cpt = 0
-        while True:
-            # r = random.choice(choices)  # the number of members of the team
-            r = choices[-1]
-            if teamsCopy[r] > 0:
-                teamsCopy[r] -= 1
-            else:
-                choices.remove(r)
-                if len(choices) == 0:
-                    break
-                continue
- 
-            cpt += 1
-            delivery = [r]
-            deliveryIngredients = set()
-            for _ in range(r):
-                delivery.append(pizzasSorted[b][0])
-                deliveryIngredients.update(pizzasSorted[b][1])
-                b += 1
-            deliveries.append(delivery)
-            score += len(deliveryIngredients)**2
- 
-            N -= r
- 
-            if N <= 1:
+    # for i in range(numberOfIterations):
+    #     if i == values[0]:
+    #         values.pop(0)
+    #         print("iteration {}".format(i))
+    # teams = teams.copy()
+
+    # b = 0
+    N = M
+    value = 5
+    score = 0
+    cpt = 0
+    deliveries = []
+    choices = [2, 3, 4]
+    while True:
+        r = choices[-1]
+        if teams[r] > 0:
+            teams[r] -= 1
+        else:
+            choices.remove(r)
+            if len(choices) == 0:
                 break
- 
-            if N <= 4:
-                cpt += 1
+            continue
+
+        # cpt += 1
+        delivery = [r]
+        s = 0
+
+        delivery.append(pizzasSorted[0][0])
+        deliveryIngredients = set(pizzasSorted[0][1])
+
+        for _ in range(r-1):
+            for j in range(1, len(pizzasSorted)):
+                
+                deliveryIngredientsCopy = deliveryIngredients.copy()
+                deliveryIngredientsCopy.update(pizzasSorted[j][1])
+
+                t = len(deliveryIngredients)
+
+                if t > s:
+                    s = t
+                    meillurePizza = pizzasSorted[j][0]
+                    k = j
+
+            delivery.append(meillurePizza)
+            deliveryIngredients.update(pizzasSorted[k][1])
+            pizzasSorted.pop(k)
+            
+        pizzasSorted.pop(0)
+        deliveries.append(delivery)
+        cpt += 1
+        score += s**2
+        if cpt == value:
+            print("I have done {} deliveries so far (score = {})".format(value, score))
+            value += 5
+
+        N -= r
+
+        if N <= 1:
+            break
+
+        if N <= 4: # N in [2,3,4]
+            if teams[N] > 0:
                 delivery = [N]
                 deliveryIngredients = set()
                 for _ in range(N):
-                    delivery.append(pizzasSorted[b][0])
-                    deliveryIngredients.update(pizzasSorted[b][1])
-                    b += 1
+                    delivery.append(pizzasSorted[0][0])
+                    deliveryIngredients.update(pizzasSorted[0][1])
+                    pizzasSorted.pop(0)
                 deliveries.append(delivery)
+                cpt += 1
                 score += len(deliveryIngredients)**2
                 break
- 
-        if score > bestScore:
-            bestScore = score
-            try:
-                os.remove("drafts/{}/{}".format(
-                    nameOfFile,
-                    os.listdir("drafts/{}".format(nameOfFile))[0]))
-            except IndexError:
-                pass
-            with open("drafts/{0}/{0} {1}.txt".format(nameOfFile, score), "w") as file:
-                print(cpt, file=file)
-                for delivery in deliveries:
-                    print(*delivery, sep=" ", file=file)
-                # delete the last newline
-                file.truncate(file.tell() - len(os.linesep))
+            else:
+                break
+
+    if score > bestScore:
+        bestScore = score
+        try:
+            os.remove("drafts/{}/{}".format(
+                nameOfFile,
+                os.listdir("drafts/{}".format(nameOfFile))[0]))
+        except IndexError:
+            pass
+        with open("drafts/{0}/{0} {1}.txt".format(nameOfFile, score), "w") as file:
+            print(len(deliveries), file=file)
+            for delivery in deliveries:
+                print(*delivery, sep=" ", file=file)
+            # delete the last newline
+            file.truncate(file.tell() - len(os.linesep))
  
     print("best score after {} iterations ({}) =".format(numberOfIterations, nameOfFile), bestScore)
     print("---- %s seconds ----" % (time.time() - start_time))
