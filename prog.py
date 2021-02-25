1ba5625f1a55a3e136eddcf00234693df8b581a3
Abdesselam Benameur <ia_benameur@esi.dz>
2021-02-25 17:00:28 +0100

    Create plots for the datasets

--- prog.py
+++ prog.py
@@ -2,6 +2,7 @@ import itertools
 import time
 import random
 import os
+import matplotlib.pyplot as plt 
 
 numberOfIngredients_dict = {
     "a_example": 7,
@@ -12,11 +13,11 @@ numberOfIngredients_dict = {
 }
 
 names = [
-    "a_example",
+    # "a_example",
     "b_little_bit_of_everything",
-    "c_many_ingredients",
-    "d_many_pizzas",
-    "e_many_teams",
+    # "c_many_ingredients",
+    # "d_many_pizzas",
+    # "e_many_teams",
 ]
 
 scoreTotal = 0
@@ -36,50 +37,41 @@ for name in names:
 
 for nameOfFile in names:
     start_time = time.time()
-    
+    x, y = [], []
     allIngredients = {}
-    with open("drafts/ingredients/{} ingredients.txt".format(nameOfFile)) as file:
+    ingredients_dict = {}
+    with open("drafts/ingredients_stats/{} ingredients.txt".format(nameOfFile)) as file:
         file.readline()
-        i = 0
-        for ingredient in file:
-            allIngredients[ingredient.rstrip()] = i
-            i += 1
-
-    with open("{}.in".format(nameOfFile)) as file:
-        firstLine = list(map(int, file.readline().strip().split(" ")))
-        M = firstLine[0]  # number of pizzas
-        teams = {2: firstLine[1], 3: firstLine[2], 4: firstLine[3]}
- 
-        pizzas = []
-        pizzas_dict = {}
-        ingredients_dict = {}
-        for i in range(M):
-            line = file.readline().strip().split(" ")
-            numberOfIngredients = int(line[0])
-            ingredients = line[1:]
-            pizzas_dict[i] = [0] * numberOfIngredients_dict[nameOfFile]
-            for ingredient in ingredients:
-                pizzas_dict[i][allIngredients[ingredient]] = 1
-                # try:
-                #     ingredients_dict[ingredient].append(i)
-                # except KeyError:
-                #     ingredients_dict[ingredient] = [i]
-            ingredients.sort()
-            pizzas.append(ingredients)
+        for line in file:
+            line = line.split()[1:]
+            ingredient = line[0]
+            l = list(map(int, line[1:]))
+            ingredients_dict[ingredient] = l
+            y += l
+            x += [ingredient]*len(l)
     
+    # plotting the points
+    plt.plot(x, y, linestyle='', marker=',')
+
+    # naming the x axis x²
+    plt.xlabel('pizzas') 
+    # naming the y axis 
+    plt.ylabel('ingredients') 
+    plt.title(nameOfFile)
+    plt.show()
     # pizzasSorted = sorted(enumerate(pizzas), key=lambda e: (len(e[1]), e[1]), reverse=True)
     # allIngredients = sorted(ingredients_dict)
 
-    with open("drafts/matrices/{} matrix.csv".format(nameOfFile), "w") as file:
-        print("Number of Pizzas ({}) x Number of ingredients ({})".format(M, len(allIngredients)),  file=file)
-        print(" ", *allIngredients, sep=', ', file=file)
-        for i in range(M):
-            print(i, end=", ", file=file)
-            print(*pizzas_dict[i], sep=', ', file=file)
-    print("---- %s seconds ----" % (time.time() - start_time))
-        # for ingredient in allIngredients:
-        #     ingredients_dict[ingredient].sort()
-        #     print(len(ingredients_dict[ingredient]), ingredient , *ingredients_dict[ingredient], sep=" ", file=file)
+    # with open("drafts/matrices/{} matrix.csv".format(nameOfFile), "w") as file:
+    #     print("Number of Pizzas ({}) x Number of ingredients ({})".format(M, len(allIngredients)),  file=file)
+    #     print(" ", *allIngredients, sep=', ', file=file)
+    #     for i in range(M):
+    #         print(i, end=", ", file=file)
+    #         print(*pizzas_dict[i], sep=', ', file=file)
+    # print("---- %s seconds ----" % (time.time() - start_time))
+    #     # for ingredient in allIngredients:
+    #     #     ingredients_dict[ingredient].sort()
+    #     #     print(len(ingredients_dict[ingredient]), ingredient , *ingredients_dict[ingredient], sep=" ", file=file)
     
 
     # with open("drafts/ingredients/{0} ingredients.txt".format(nameOfFile), "w") as file:
--- script.py
+++ script.py
@@ -1,16 +1,54 @@
-import datetime 
-alphabets = [str(x)for x in range(10000000)] 
-a = datetime.datetime.now() # store initial time 
-for item in alphabets: 
-    len(item) 
-b = datetime.datetime.now() # store final time 
-print((b-a).total_seconds()) # results 
-a = datetime.datetime.now() 
-fn = len                    # function stored locally 
-for item in alphabets: 
-    fn(item) 
-b = datetime.datetime.now() 
-print((b-a).total_seconds())
+#%%
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
+import numpy as np
+#%%
+Z = [[1,2], [5,6]] # np.random.rand(6, 10)
+x = np.arange(-0.5, 10, 1)  # len = 11
+y = np.arange(4.5, 11, 1)  # len = 7
+
+#%%
+fig, ax = plt.subplots()
+ax.pcolormesh(Z)
+#%%
+
+# import matplotlib.pyplot as plt 
+  
+# # x axis values 
+# x = [1,2,3] 
+# # corresponding y axis values 
+# y = [2,4,1] 
+  
+# # plotting the points  
+# plt.plot(x, y) 
+  
+# # naming the x axis 
+# plt.xlabel('x - axis') 
+# # naming the y axis 
+# plt.ylabel('y - axis') 
+  
+# # giving a title to my graph 
+# plt.title('My first graph!') 
+  
+# # function to show the plot 
+# plt.show() 
+
+
+# import datetime 
+# alphabets = [str(x)for x in range(10000000)] 
+# a = datetime.datetime.now() # store initial time 
+# for item in alphabets: 
+#     len(item) 
+# b = datetime.datetime.now() # store final time 
+# print((b-a).total_seconds()) # results 
+# a = datetime.datetime.now() 
+# fn = len                    # function stored locally 
+# for item in alphabets: 
+#     fn(item) 
+# b = datetime.datetime.now() 
+# print((b-a).total_seconds())
 
 
 # import datetime
